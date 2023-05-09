f1da6afbc3b1e992aee8de631dda52e5521030e4
Rwagaso <stevenrwa31@gmail.com>
2023-05-09 11:32:00 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -16,6 +16,7 @@ int main(int ac, char **av)
         dprintf(2, "Usage: %s filename text\n", av[0]);
         exit(1);
     }
-    res = appends _text_to_file(av[1], av[2]);
+    res = appends_text_to_file(av[1], av[2]);
     printf("-> %i)\n", res);
-    return }
+    return (0);
+}
